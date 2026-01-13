37dac9ab4f63f8c30be6736e1d789042b8d5d5da
Andrew Koulogeorge <Andrew.j.koulogeorge.24@dartmouth.edu>
2026-01-13 13:32:41 -0500

    bug fix vector add

--- ch2/vector_add.cu
+++ ch2/vector_add.cu
@@ -7,8 +7,45 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
+// device method that computes element wise addition between vectors
+ __global__ 
+ void vecAddKernel(float *A, float *B, float *C, int n){
+    // compute idx of this thread based on block.size, block.idx and thread.idx
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n){
+        C[i] = A[i] + B[i];
+    }
+ }
 
- // main method to allocate arrays on host to be added together
+ // host method vecAdd to handle device allocation and copying
+ void vecAdd(float *A_h, float *B_h, float *C_h, int n){
+    
+    // allocate memory on the GPU for A_d,B_d,C_d
+    int size = n*sizeof(float);
+    float *A_d, *B_d, *C_d;
+    cudaMalloc((void **) &A_d, size);
+    cudaMalloc((void **) &B_d, size);
+    cudaMalloc((void **) &C_d, size);
+
+    // copy memory from host to device
+    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+    // launch kernel with correct number of blocks
+    int block_size = 32;
+    int num_blocks = ceil(n / (double)block_size);
+    vecAddKernel<<<num_blocks, block_size>>>(A_d, B_d, C_d, n);
+
+    // copy memory of output C_d -> C_h
+    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+
+    // free all memory on device
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+ }
+
+  // main method to allocate arrays on host to be added together
  int main(int argc, char* argv[]){
     if (argc != 2){
         return -1;
@@ -38,44 +75,4 @@
         printf("%.0f + %.0f = %.0f\n", A_h[i], B_h[i], C_h[i]);
     }
     return 0;
- }
-
- // host method vecAdd to handle device allocation and copying
- void vecAdd(float *A_h, float *B_h, float *C_h, int n){
-    
-    // allocate memory on the GPU for A_d,B_d,C_d
-    int size = n*sizeof(float);
-    float *A_d, *B_d, *C_d;
-    cudaMalloc((void **) &A_d, size);
-    cudaMalloc((void **) &B_d, size);
-    cudaMalloc((void **) &C_d, size);
-
-    // copy memory from host to device
-    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
-
-    // launch kernel with correct number of blocks
-    int block_size = 32;
-    int num_blocks = ceil(n / (double)block_size);
-    vecAddKernel<<<num_blocks, block_size>>>(A_d, B_d, C_d, n);
-
-    // copy memory of output C_d -> C_h
-    cudaMemCopy(C_h, C_d, size, cudaMemcpyDeviceToHost);
-
-    // free all memory on device
-    cudaFree(A_d);
-    cudaFree(B_d);
-    cudaFree(C_d);
-    return 0;
-
- }
-
- // device method that computes element wise addition between vectors
- __global__ 
- void vecAddKernel(float *A, float *B, float *C, int n){
-    // compute idx of this thread based on block.size, block.idx and thread.idx
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n){
-        C[i] = A[i] + B[i];
-    }
  }
